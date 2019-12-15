084267205c8826b36a05acdbba6bfdd4707cca2d
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-12-15 23:41:33 +0300

    Add an instructions dialog

--- app/src/main/java/dev/valeryvpetrov/findme/MainActivity.kt
+++ app/src/main/java/dev/valeryvpetrov/findme/MainActivity.kt
@@ -1,12 +1,28 @@
 package dev.valeryvpetrov.findme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dialog = createInstructionsDialog()
+        dialog.show()
+    }
+
+
+    private fun createInstructionsDialog(): Dialog {
+        return AlertDialog.Builder(this)
+            .setIcon(R.drawable.android)
+            .setTitle(R.string.instructions_title)
+            .setMessage(R.string.instructions)
+            .setPositiveButtonIcon(ContextCompat.getDrawable(this, android.R.drawable.ic_media_play))
+            .create()
     }
 }
